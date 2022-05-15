7b0ff1ba36bcb13e2e02ba1c87dd659e9a1080f8
noolisainathareddy <nooli.sainathareddy@gmail.com>
2022-05-15 08:04:30 -0500

    added new lines of code

--- sample/src/main/java/com/sai/sample/addition.java
+++ sample/src/main/java/com/sai/sample/addition.java
@@ -1,11 +1,21 @@
 package com.sai.sample;
 
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.values.PCollection;
+
+
 public class addition {
 	public static void main(String[] args)
 	{
-		int a = 5;
-		int b = 6;
+		Pipeline p=Pipeline.create();
 		
-		System.out.println("sum :" + (b-a));
+		PCollection<String> outCollection=p.apply(TextIO.read().from("/Users/sainooli/eclipse-workspace/dataflow/input/input1.csv"));
+		
+		outCollection.apply(TextIO.write().to("/Users/sainooli/eclipse-workspace/dataflow/output/output1"));
+		
+		//System.out.println("sai");
+		
+		p.run(); 
 	}
 }
