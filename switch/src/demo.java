4fbb27bbc2056e54e820a2e508342e8ea4c63631
noolisainathareddy <nooli.sainathareddy@gmail.com>
2022-05-28 11:44:38 -0500

    switch case example

---
+++ switch/src/demo.java
@@ -0,0 +1,20 @@
+
+public class demo {
+
+	public static void main(String[] args)
+	{
+		int score = 4; 
+		
+		switch(score)
+		{
+		case 1:
+			System.out.println("this is number one"); 
+			break; 
+		case 4: 
+			System.out.println("this is number four"); 
+			break;
+		default: 
+			System.out.println("inavlid");
+		}
+	}
+}
