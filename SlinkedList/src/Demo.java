4a1972685dd5827740bcd73fd6299903c8bf2991
noolisainathareddy <nooli.sainathareddy@gmail.com>
2022-06-26 23:02:59 -0500

    adding data base programs to repo

--- SlinkedList/src/Demo.java
+++ SlinkedList/src/Demo.java
@@ -3,37 +3,54 @@
 class ListNode
 {
 	int data; 
-	ListNode next; 
+	ListNode next;
 	
 	public ListNode(int data)
 	{
 		this.data=data; 
-		this.next=null;
+		this.next=null; 
 	}
 }
 public class Demo {
-
-	private ListNode head; 
 	
+	public ListNode head; 
+
 	public static void main(String[] args)
 	{
 		Demo sll=new Demo();
-		sll.head=new ListNode(10); 
-		ListNode second=new ListNode(20); 
-		ListNode third = new ListNode(30);
-		ListNode fourth = new ListNode(40);
 		
-		sll.head.next=second;
+		sll.head = new ListNode(10); 
+		ListNode second = new ListNode(20); 
+		ListNode third  = new ListNode(30); 
+		ListNode fourth = new ListNode(30); 
+		ListNode fifth  = new ListNode(50); 
+		
+		sll.head.next=second; 
 		second.next=third; 
 		third.next=fourth; 
+		fourth.next=fifth; 
 		
 		ListNode current = sll.head;
-		while(current != null)
+		while(current !=null)
 		{
 			System.out.print(current.data + ",");
+			try {
+				if(current.data == current.next.data)
+				{
+					current=current.next;  
+					
+					
+				}
+			}
+			catch(Exception e)
+			{
+				//System.out.print("errorr......");
+			}
+			
 			current = current.next; 
 		}
+		
 		System.out.print("null");
+		
 	}
-
 }
--- dummy2/src/main/java/dummy2/Demo.java
+++ dummy2/src/main/java/dummy2/Demo.java
@@ -8,9 +8,18 @@ public class Demo {
 	public static void main(String[] args)
 	{
 		
-		double now=System.currentTimeMillis();
-		
-		System.out.println(now);
+		int num = 1221;
+		int pal=0; 
+		while(num > 0)
+		{
+			pal = pal * 10 + (num % 10); 
+			
+			
+			num = num/10; 
+			//System.out.println(num);
+			
+		}
+		System.out.println(pal);
 	}
 
 }
